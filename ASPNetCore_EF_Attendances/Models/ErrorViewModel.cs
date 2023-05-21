cb18bf04371774d1e8a727c391770582c85dca68
WimHambrouck-EhB <wim.hambrouck@ehb.be>
2023-05-21 09:57:40 +0200

    Add project files.

---
+++ ASPNetCore_EF_Attendances/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPNetCore_EF_Attendances.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
